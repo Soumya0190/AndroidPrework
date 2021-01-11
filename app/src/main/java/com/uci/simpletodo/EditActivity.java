7d7a17fcf2fae12248198292ae9badbde8d0e57e
Soumya0190 <ssoumya0119@gmail.com>
2021-01-11 00:58:39 -0800

    Completed prework

---
+++ app/src/main/java/com/uci/simpletodo/EditActivity.java
@@ -0,0 +1,26 @@
+package com.uci.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity
+{
+    EditText etItem;
+    Button btnSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        etItem = findViewById(R.id.updateItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit Item ");
+    }
+}
